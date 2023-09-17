bd5760b4e15a237629109f8d1e58ab22cca062e5
jasopilami <Jascha.Djalal@web.de>
2023-09-18 00:44:13 +0200

    added data structure

--- backend/src/db.js
+++ backend/src/db.js
@@ -11,6 +11,10 @@ const User = sequelize.define("User", {
     autoIncrement: true,
     primaryKey: true,
   },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
@@ -20,11 +24,97 @@ const User = sequelize.define("User", {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  role: {
-    type: DataTypes.ENUM("user", "trainer"),
-    defaultValue: "user",
+});
+
+const News = sequelize.define("News", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  message: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
 });
+
+const Trainer = sequelize.define("Trainer", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  avatar: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+const Session = sequelize.define("Session", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  dateTime: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  timeInMinutes: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+const Booking = sequelize.define("Booking", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+});
+
+Trainer.hasMany(Session, { as: "sessions " });
+Session.belongsTo(Trainer);
+
+User.hasMany(Booking, { as: "bookings" });
+Booking.belongsTo(User);
+
+Session.hasMany(Booking, { as: "bookings" });
+Booking.belongsTo(Session);
 
 sequelize.sync({ force: true }).then(() => {
   console.log("Database & tables synced");
